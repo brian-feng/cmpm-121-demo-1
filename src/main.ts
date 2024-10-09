b15bd8d6ce37322008b1040ed5c3e9ed70196fc2
Brian Feng <brainfeng135@gmail.com>
2024-10-09 14:30:06 -0700

    Step 1

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,7 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button: HTMLButtonElement = <HTMLButtonElement>document.createElement("button");
+button.textContent = "press me";
+app.append(button);
