36d511b8dc40831c449419f36c2e2cc7d427860b
brian feng <42507580+brian-feng@users.noreply.github.com>
2024-10-10 02:51:55 +0000

    Step 2 (and no more boba its now about GAINS)

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,25 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing super cool game";
+const gameName = "Lifty Clicker";
 document.title = gameName;
-
 const header = document.createElement("h1");
-header.innerHTML = gameName;
+header.innerHTML = "CLICK TO LIFT";
 app.append(header);
+
+let gains: number = 0;
+const gainsText = document.createElement("h2");
+gainsText.innerHTML = "GAINS: " + gains.toString();
+app.append(gainsText);
 
 const button: HTMLButtonElement = <HTMLButtonElement>(
   document.createElement("button")
 );
-button.textContent = "🧋";
+button.textContent = "💪";
 app.append(button);
+
+button.onclick = () => {
+  gains += 1;
+  gainsText.innerHTML = "GAINS: " + gains.toString();
+  console.log(gains);
+};
