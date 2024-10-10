976d2949c77f89da65cee1f0c97a8f95f6f2aed3
brain.exe <brainfeng135@gmail.com>
2024-10-09 23:59:43 -0700

    Step 3

--- src/main.ts
+++ src/main.ts
@@ -6,26 +6,26 @@ document.title = gameName;
 let gains: number = 0;
 
 function makeHeader(): HTMLElement {
-  const header = document.createElement("h1");
-  header.innerHTML = "CLICK TO LIFT";
-  return header;
+	const header = document.createElement("h1");
+	header.innerHTML = "CLICK TO LIFT";
+	return header;
 }
 
 function makeGainsText(): HTMLElement {
-  const gainsText = document.createElement("h2");
-  gainsText.innerHTML = "GAINS: " + gains.toString();
-  return gainsText;
+	const gainsText = document.createElement("h2");
+	gainsText.innerHTML = "GAINS: " + gains.toString();
+	return gainsText;
 }
 
 function makeButton(): HTMLButtonElement {
-  const button: HTMLButtonElement = <HTMLButtonElement>(
-    document.createElement("button")
-  );
-  button.textContent = "💪";
-  button.onclick = () => {
-    new Event("click");
-  };
-  return button;
+	const button: HTMLButtonElement = <HTMLButtonElement>(
+    	document.createElement("button")
+  	);
+	button.textContent = "💪";
+	button.onclick = () => {
+    	new Event("click");
+	};
+	return button;
 }
 
 const gainsText: HTMLElement = makeGainsText();
@@ -34,12 +34,17 @@ app.append(makeHeader());
 app.append(gainsText);
 app.append(button);
 
+function increaseGains() {
+	gains += 1;
+	gainsText.innerHTML = "GAINS: " + gains.toString();
+	console.log(gains);
+}
+
 app.addEventListener(
-  "click",
-  () => {
-    gains += 1;
-    gainsText.innerHTML = "GAINS: " + gains.toString();
-    console.log(gains);
-  },
-  false,
+	"click",
+	increaseGains,
+	false,
 );
+
+const cps: number = 1;
+setInterval(increaseGains, 1000/cps);
